f936af9ef4665893cf58ab5139e31c1d6aee0b9f
Bas Pennings <baspster@gmail.com>
2019-09-12 21:32:14 +0100

    Tuning

--- src/438/tuning.jl
+++ src/438/tuning.jl
@@ -2,11 +2,16 @@
 # A4₄ = 438 Hz
 
 C₀ = Note(16.28)
+C♯₀ = Note(17.25)
 D₀ = Note(18.27)
+D♯₀ = Note(19.36)
 E₀ = Note(20.51)
 F₀ = Note(21.73)
+F♯₀ = Note(23.02)
 G₀ = Note(24.39)
+G♯₀ = Note(25.84)
 A₀ = Note(27.38)
+A♯₀ = Note(29.00)
 B₀ = Note(30.37)
 
 C₄ = Note(260.44)
--- src/Lydia.jl
+++ src/Lydia.jl
@@ -2,18 +2,37 @@ module Lydia
 
 using Unitful
 
-abstract type AbstractTuning end
-
 const Ω = 345u"m/s"
 
 λ(f) = Ω / f
 
-lerp(a, b, t) = (1 - t) * a + t * b
+abstract type AbstractNote end
 
-struct Note
+f(x::AbstractNote) = x.f
+λ(x::AbstractNote) = x.λ
+
+struct Note : AbstractNote
     f   # frequency (Hz)
     λ   # wavelength (m)
 end
+
+abstract type AbstractTuning end
+
+function C₀(t::AbstractTuning):: AbstractNote
+function C♯₀(t::AbstractTuning):: AbstractNote
+
+function D₀ = Note(18.27)
+function D♯₀ = Note(19.36)
+function E₀ = Note(20.51)
+function F₀ = Note(21.73)
+function F♯₀ = Note(23.02)
+function G₀ = Note(24.39)
+function G♯₀ = Note(25.84)
+function A₀ = Note(27.38)
+function A♯₀ = Note(29.00)
+function B₀ = Note(30.37)
+
+lerp(a::Note, b::Note, t) = (1 - t) * a.f + t * b.f |> Note
 
 Note(f) = Note(f, λ(f))
 
